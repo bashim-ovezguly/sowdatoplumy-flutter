dd12034d9ebca6030ebbbf157f7c909f60793b93
babajanshamyradow <babajanshamyradow98@gmail.com>
2023-09-22 10:57:24 +0500

    bug fixed

--- lib/pages/Store/checkout.dart
+++ lib/pages/Store/checkout.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import, must_be_immutable
+// ignore_for_file: unused_import, must_be_immutable, unused_local_variable
 
 import 'dart:convert';
 import 'package:intl/intl.dart';
