cfe555dd0aa228fcf29f26ff2f7f303927c68438
babajanshamyradow <babajanshamyradow98@gmail.com>
2023-09-22 10:56:43 +0500

    bug fixed

--- lib/pages/Customer/RealEstate/getFirst.dart
+++ lib/pages/Customer/RealEstate/getFirst.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_field
+
 import 'dart:convert';
 import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
 import 'package:http/http.dart' as http;
--- lib/pages/Store/checkout.dart
+++ lib/pages/Store/checkout.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, must_be_immutable
 
 import 'dart:convert';
 import 'package:intl/intl.dart';
